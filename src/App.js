22854c57372333391541711a5ed4047827e32a25
Aleksandr <adorofeev@team.amocrm.com>
2020-07-28 20:11:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,135 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import { format, addDays } from "date-fns";
+import _ from "lodash";
 
-function App() {
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [date, setDate] = useState(addDays(new Date(), -90));
+  const [counter, setCounter] = useState(90);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const [responseUSD, responseEUR] = await Promise.all([
+        axios.get(
+          `https://api.exchangeratesapi.io/${format(
+            date,
+            "yyyy-MM-dd"
+          )}?base=USD&symbols=RUB`
+        ),
+        axios.get(
+          `https://api.exchangeratesapi.io/${format(
+            date,
+            "yyyy-MM-dd"
+          )}?base=EUR&symbols=RUB`
+        ),
+      ]);
+
+      const dataNew = [].concat(
+        convertRates(responseUSD.data),
+        convertRatesEUR(responseEUR.data)
+      );
+
+      const grouped = _.groupBy(dataNew, "time");
+      for (const [key, value] of Object.entries(grouped)) {
+        const row = { time: key };
+        for (const item of value) {
+          if (Object.keys(item)[1] === "USD") {
+            row[Object.keys(item)[1]] = item.USD;
+          }
+          if (Object.keys(item)[1] === "EUR") {
+            row[Object.keys(item)[1]] = item.EUR;
+          }
+        }
+        setData([...data, row]);
+      }
+    };
+    fetchData();
+  }, [date]);
+
+  const convertRates = (rates) => {
+    return {
+      time: format(new Date(rates["date"]), "dd.MM"),
+      USD: rates["rates"].RUB,
+    };
+  };
+
+  const convertRatesEUR = (rates) => {
+    return {
+      time: format(new Date(rates["date"]), "dd.MM"),
+      EUR: rates["rates"].RUB,
+    };
+  };
+
+  useEffect(() => {
+    const timer =
+      counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+    setDate(addDays(new Date(), -counter));
+    return () => clearInterval(timer);
+  }, [counter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="chart">
+      {console.log(data)}
+      <div>
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            bottom: 5,
+            left: -20,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <CartesianGrid strokeDasharray="1 1" />
+          <XAxis dataKey="time" domain={["auto", "auto"]} />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="linear"
+            dataKey="EUR"
+            stroke="#8884d8"
+            dot={false}
+            isAnimationActive={false}
+          />
+          <Line
+            type="linear"
+            dataKey="USD"
+            stroke="#82ca9d"
+            dot={false}
+            isAnimationActive={false}
+          />
+        </LineChart>
+
+        <div className="table">
+          <div>Дата</div>
+          <div>USD</div>
+          <div>EUR</div>
+          {data !== undefined &&
+            data.map((item) => (
+              <>
+                <div>{item.time}</div>
+                <div>{item.USD}</div>
+                <div>{item.EUR}</div>
+              </>
+            ))}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
