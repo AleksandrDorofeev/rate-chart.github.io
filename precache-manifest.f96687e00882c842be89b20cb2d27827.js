c85b99805a485898573f5dfad10c8993d84bbb59
Aleksandr <adorofeev@team.amocrm.com>
2020-07-28 20:19:03 +0300

    Updates

---
+++ precache-manifest.f96687e00882c842be89b20cb2d27827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b2622690b7fefb9d339a8c6eac7c3c",
+    "url": "/chart/index.html"
+  },
+  {
+    "revision": "b97d9559f3d2a39868f6",
+    "url": "/chart/static/css/main.5b876b2b.chunk.css"
+  },
+  {
+    "revision": "a8967cdd8c0dfd5814f3",
+    "url": "/chart/static/js/2.35f56b74.chunk.js"
+  },
+  {
+    "revision": "f2a020ebcdd97c593a441d685b589e20",
+    "url": "/chart/static/js/2.35f56b74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b97d9559f3d2a39868f6",
+    "url": "/chart/static/js/main.a7734458.chunk.js"
+  },
+  {
+    "revision": "438404099aa63b9c7c14",
+    "url": "/chart/static/js/runtime-main.b3a89dee.js"
+  }
+]);
